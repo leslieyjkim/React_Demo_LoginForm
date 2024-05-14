9305754361c96463f637c176123ed56968b629cc
yoonjookim <yjblossom@gmail.com>
2024-05-14 16:34:22 -0400

    Update 1st.component 'ProductListItem' and styling

--- Demo_LoginForm/src/components/ProductListItem.jsx
+++ Demo_LoginForm/src/components/ProductListItem.jsx
@@ -12,17 +12,17 @@ const ProductListItem = (props) => {
     <div className="product-list-item">
       <div className="product-header">
         <h2>Product: { props.name }</h2>
-        <h3 className="product-price">Price: ${ props.price }</h3>
+        <h3 className="product-price">Price: $ { props.price }</h3>
       </div>
       <hr />
       <h3>Description: { props.description }</h3>
     </div>
   );
 }
-
+//defaultProps object
 ProductListItem.defaultProps = {
   name: 'Chocolate bar',
-  price: 0.99,
+  price: 7.99,
   description: 'Milk chocolate with the seasalt and caramel'
 };
 
